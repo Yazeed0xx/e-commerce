e29f0126bcd1920aef6eb82a8285d67556f08369
Yazeed0xx <yazeedthunyan@gmail.com>
2025-07-13 00:18:29 +0300

    Remove server folder from tracking and add to .gitignore

--- apps/server/src/db/migrations/0001_good_black_cat.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `posts` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`title` varchar(255) NOT NULL,
-	`content` varchar(1000) NOT NULL,
-	CONSTRAINT `posts_id` PRIMARY KEY(`id`)
-);
--- apps/server/src/db/migrations/0002_thankful_kid_colt.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `comments` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`postId` int NOT NULL,
-	`content` varchar(1000) NOT NULL,
-	CONSTRAINT `comments_id` PRIMARY KEY(`id`)
-);
